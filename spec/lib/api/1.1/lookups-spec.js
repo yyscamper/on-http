90aaec6b194aed2d2846955e3d37316c7d33560b
Felix Yuan <felix.yuan@emc.com>
2016-05-24 22:40:53 +0800

    add PUT /lookups API to fix esx-ks installation timeout issue

--- spec/lib/api/1.1/lookups-spec.js
+++ spec/lib/api/1.1/lookups-spec.js
@@ -4,7 +4,7 @@
 'use strict';
 
 describe('Http.Api.Lookup', function () {
-    var waterline, stub;
+    var waterline, sandbox;
 
     var data = [
         {
@@ -27,6 +27,7 @@ describe('Http.Api.Lookup', function () {
 
     before('start HTTP server', function () {
         this.timeout(5000);
+        sandbox = sinon.sandbox.create();
         return helper.startServer();
     });
 
@@ -35,10 +36,7 @@ describe('Http.Api.Lookup', function () {
     });
 
     afterEach(function () {
-        if (stub) {
-            stub.restore();
-            stub = undefined;
-        }
+        sandbox.restore();
     });
 
     after('stop HTTP server', function () {
@@ -48,38 +46,180 @@ describe('Http.Api.Lookup', function () {
     describe('/api/1.1/lookups', function () {
         describe('GET', function () {
             it('should should call waterline.lookups.findByTerm', function() {
-                stub = sinon.stub(waterline.lookups, 'findByTerm').resolves(data);
+                sandbox.stub(waterline.lookups, 'findByTerm').resolves(data);
 
                 return helper.request().get('/api/1.1/lookups')
                     .expect('Content-Type', /^application\/json/)
                     .expect(200)
                     .expect(function () {
-                        expect(stub).to.have.been.calledWith(undefined);
+                        expect(waterline.lookups.findByTerm).to.have.been.calledWith(undefined);
                     });
             });
 
             it('should call waterline.lookups.findByterm with 123', function() {
-                stub = sinon.stub(waterline.lookups, 'findByTerm').resolves(data);
+                sandbox.stub(waterline.lookups, 'findByTerm').resolves(data);
 
                 return helper.request().get('/api/1.1/lookups?q=123')
                     .expect('Content-Type', /^application\/json/)
                     .expect(200)
                     .expect(function () {
-                        expect(stub).to.have.been.calledWith('123');
+                        expect(waterline.lookups.findByTerm).to.have.been.calledWith('123');
                     });
             });
         });
 
         describe('POST', function () {
             it('should call waterline.lookups.create', function() {
-                stub = sinon.stub(waterline.lookups, 'create').resolves(data[0]);
+                sandbox.stub(waterline.lookups, 'create').resolves(data[0]);
 
                 return helper.request().post('/api/1.1/lookups')
                     .send(data[0])
                     .expect('Content-Type', /^application\/json/)
                     .expect(200)
                     .expect(function () {
-                        expect(stub).to.have.been.calledWith(sinon.match(data[0]));
+                        expect(waterline.lookups.create).to.been.calledWith(sinon.match(data[0]));
+                    });
+            });
+        });
+
+        describe('PUT', function () {
+            it('should create a new lookup entry', function() {
+                var entry = data[0];
+                sandbox.stub(waterline.lookups, 'findOne').resolves();
+                sandbox.stub(waterline.lookups, 'create',
+                    function(data) {
+                        var result = _.cloneDeep(data);
+                        result.id = '112233445566';
+                        return Promise.resolve(result);
+                    }
+                );
+                sandbox.spy(waterline.lookups, 'updateOneById');
+
+                return helper.request().put('/api/1.1/lookups')
+                    .send(entry)
+                    .expect('Content-Type', /^application\/json/)
+                    .expect(200)
+                    .expect(function (data) {
+                        expect(waterline.lookups.findOne).to.have.been.called;
+                        expect(waterline.lookups.updateOneById).to.not.have.been.called;
+                        expect(waterline.lookups.create).to.have.callCount(1)
+                            .and.been.calledWith(entry);
+                        expect(data.body).to.have.property('ipAddress')
+                            .to.equal(entry.ipAddress);
+                        expect(data.body).to.have.property('macAddress')
+                            .to.equal(entry.macAddress);
+                        expect(data.body).to.have.property('node')
+                            .to.equal(entry.node);
+                        expect(data.body).to.have.property('id')
+                            .to.equal('112233445566');
+                    });
+            });
+
+            it('should replace the record if there is duplicated macAddress', function() {
+                var origData = data[0];
+                var newData = {
+                    ipAddress: '172.31.128.98',
+                    macAddress: origData.macAddress,
+                    node: origData.node
+                };
+                sandbox.stub(waterline.lookups, 'findOne').resolves(origData);
+                sandbox.stub(waterline.lookups, 'updateOneById',
+                    function(id, data) {
+                        var result = _.cloneDeep(data);
+                        result.id = id;
+                        return Promise.resolve(result);
+                    }
+                );
+                sandbox.spy(waterline.lookups, 'create');
+                return helper.request().put('/api/1.1/lookups')
+                    .send(newData)
+                    .expect('Content-Type', /^application\/json/)
+                    .expect(200)
+                    .expect(function(data) {
+                        expect(waterline.lookups.findOne).to.have.been.called;
+                        expect(waterline.lookups.updateOneById).to.have.callCount(1)
+                            .and.been.calledWith(origData.id, newData);
+                        expect(waterline.lookups.create).to.not.have.been.called;
+                        expect(data.body).to.have.property('ipAddress')
+                            .to.equal(newData.ipAddress);
+                        expect(data.body).to.have.property('macAddress')
+                            .to.equal(newData.macAddress);
+                        expect(data.body).to.have.property('node')
+                            .to.equal(newData.node);
+                        expect(data.body).to.have.property('id')
+                            .to.equal(origData.id);
+                    });
+            });
+
+            it('should replace the record if there is duplicated ipAddress', function() {
+                var origData = data[0];
+                var newData = {
+                    ipAddress: origData.ipAddress,
+                    macAddress: '00:22:12:cc:bb:17',
+                    node: origData.node
+                };
+                sandbox.stub(waterline.lookups, 'findOne').resolves(origData);
+                sandbox.stub(waterline.lookups, 'updateOneById',
+                    function(id, data) {
+                        var result = _.cloneDeep(data);
+                        result.id = id;
+                        return Promise.resolve(result);
+                    }
+                );
+                sandbox.spy(waterline.lookups, 'create');
+                return helper.request().put('/api/1.1/lookups')
+                    .send(newData)
+                    .expect('Content-Type', /^application\/json/)
+                    .expect(200)
+                    .expect(function(data) {
+                        expect(waterline.lookups.findOne).to.have.been.called;
+                        expect(waterline.lookups.updateOneById).to.have.callCount(1)
+                            .and.been.calledWith(origData.id, newData);
+                        expect(waterline.lookups.create).to.not.have.been.called;
+                        expect(data.body).to.have.property('ipAddress')
+                            .to.equal(newData.ipAddress);
+                        expect(data.body).to.have.property('macAddress')
+                            .to.equal(newData.macAddress);
+                        expect(data.body).to.have.property('node')
+                            .to.equal(newData.node);
+                        expect(data.body).to.have.property('id')
+                            .to.equal(origData.id);
+                    });
+            });
+
+            it('should still create new record if there is duplicated node', function() {
+                var origData = data[0];
+                var newData = {
+                    ipAddress: '172.31.188.99',
+                    macAddress: '99:82:cd:76:89:12',
+                    node: origData.node
+                };
+                sandbox.stub(waterline.lookups, 'findOne').resolves();
+                sandbox.stub(waterline.lookups, 'create',
+                    function(data) {
+                        var result = _.cloneDeep(data);
+                        result.id = '112233445566';
+                        return Promise.resolve(result);
+                    }
+                );
+                sandbox.spy(waterline.lookups, 'updateOneById');
+                return helper.request().put('/api/1.1/lookups')
+                    .send(newData)
+                    .expect('Content-Type', /^application\/json/)
+                    .expect(200)
+                    .expect(function(data) {
+                        expect(waterline.lookups.findOne).to.have.been.called;
+                        expect(waterline.lookups.updateOneById).to.have.callCount(0);
+                        expect(waterline.lookups.create).to.have.callCount(1)
+                            .and.been.calledWith(newData);
+                        expect(data.body).to.have.property('ipAddress')
+                            .to.equal(newData.ipAddress);
+                        expect(data.body).to.have.property('macAddress')
+                            .to.equal(newData.macAddress);
+                        expect(data.body).to.have.property('node')
+                            .to.equal(newData.node);
+                        expect(data.body).to.have.property('id')
+                            .to.equal('112233445566');
                     });
             });
         });
@@ -88,40 +228,40 @@ describe('Http.Api.Lookup', function () {
     describe('/api/1.1/lookups/:id', function () {
         describe('GET', function () {
             it('should call waterline.lookups.needOneById with 123', function() {
-                stub = sinon.stub(waterline.lookups, 'needOneById').resolves(data[0]);
+                sandbox.stub(waterline.lookups, 'needOneById').resolves(data[0]);
 
                 return helper.request().get('/api/1.1/lookups/123')
                     .expect('Content-Type', /^application\/json/)
                     .expect(200)
                     .expect(function () {
-                        expect(stub).to.have.been.calledWith('123');
+                        expect(waterline.lookups.needOneById).to.have.been.calledWith('123');
                     });
             });
         });
 
         describe('PATCH', function () {
             it('should call waterline.lookups.updateOneById with 123', function() {
-                stub = sinon.stub(waterline.lookups, 'updateOneById').resolves(data[0]);
+                sandbox.stub(waterline.lookups, 'updateOneById').resolves(data[0]);
 
                 return helper.request().patch('/api/1.1/lookups/123')
                     .send(data[0])
                     .expect('Content-Type', /^application\/json/)
                     .expect(200)
                     .expect(function () {
-                        expect(stub).to.have.been.calledWith('123', sinon.match(data[0]));
+                        expect(waterline.lookups.updateOneById).to.have.been.calledWith('123', sinon.match(data[0]));
                     });
             });
         });
 
         describe('DELETE', function () {
             it('should call waterline.lookups.destroyOneById with 123', function() {
-                stub = sinon.stub(waterline.lookups, 'destroyOneById').resolves(data[0]);
+                sandbox.stub(waterline.lookups, 'destroyOneById').resolves(data[0]);
 
                 return helper.request().delete('/api/1.1/lookups/123')
                     .expect('Content-Type', /^application\/json/)
                     .expect(200)
                     .expect(function () {
-                        expect(stub).to.have.been.calledWith('123');
+                        expect(waterline.lookups.destroyOneById).to.have.been.calledWith('123');
                     });
             });
         });
